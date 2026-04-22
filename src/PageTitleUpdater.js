b30979a33d5a70468fe92b6069c148d292575330
Muhammad Ahmad Javed <ahmadjaved7101@gmail.com>
2026-04-22 18:36:21 +0500

    update page title to nanomight ai

--- src/PageTitleUpdater.js
+++ src/PageTitleUpdater.js
@@ -6,53 +6,53 @@ const PageTitleUpdater = () => {
 
     useEffect(() => {
         const path = location.pathname;
-        let title = 'xDial - Networks';
+        let title = 'Nanomight AI';
 
         switch (path) {
             case '/':
-                title = 'Login - XDIAL Networks';
+                title = 'Login - Nanomight AI';
                 break;
             case '/dashboard':
-                title = 'Dashboard - XDIAL Networks';
+                title = 'Dashboard - Nanomight AI';
                 break;
             case '/client-landing':
-                title = 'Landing Page - XDIAL Networks';
+                title = 'Landing Page - Nanomight AI';
                 break;
             case '/recordings':
-                title = 'Recordings - XDIAL Networks';
+                title = 'Recordings - Nanomight AI';
                 break;
             case '/data-export':
-                title = 'Data Export - XDIAL Networks';
+                title = 'Data Export - Nanomight AI';
                 break;
             case '/integration-form':
-                title = 'Integration - XDIAL Networks';
+                title = 'Integration - Nanomight AI';
                 break;
             case '/manage-team':
-                title = 'Manage Team - XDIAL Networks';
+                title = 'Manage Team - Nanomight AI';
                 break;
             case '/admin-dashboard':
-                title = 'Admin Dashboard - XDIAL Networks';
+                title = 'Admin Dashboard - Nanomight AI';
                 break;
             case '/admin-data-export':
-                title = 'Admin Data Export - XDIAL Networks';
+                title = 'Admin Data Export - Nanomight AI';
                 break;
             case '/admin-landing':
-                title = 'Admin Home - XDIAL Networks';
+                title = 'Admin Home - Nanomight AI';
                 break;
             case '/admin-server-stats':
-                title = 'Server Stats - XDIAL Networks';
+                title = 'Server Stats - Nanomight AI';
                 break;
             case '/admin-campaigns':
-                title = 'Campaigns - XDIAL Networks';
+                title = 'Campaigns - Nanomight AI';
                 break;
             case '/admin-voice-stats':
-                title = 'Voice Stats - XDIAL Networks';
+                title = 'Voice Stats - Nanomight AI';
                 break;
             case '/request-campaign':
-                title = 'Request Campaign - XDIAL Networks';
+                title = 'Request Campaign - Nanomight AI';
                 break;
             default:
-                title = 'XDIAL Networks';
+                title = 'Nanomight AI';
         }
 
         document.title = title;
